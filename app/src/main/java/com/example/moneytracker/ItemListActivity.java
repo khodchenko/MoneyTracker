55b36070224ccad262a214ada54f80498378a5d5
БОРЯ ПОГРЫЗОВ <khodch3nk0@gmail.com>
2021-01-23 18:11:14 +0100

    create new activity ItemListActivity.java create new layout activity_items_list.xml
    Signed-off-by: БОРЯ ПОГРЫЗОВ <khodch3nk0@gmail.com>

---
+++ app/src/main/java/com/example/moneytracker/ItemListActivity.java
@@ -0,0 +1,14 @@
+package com.example.moneytracker;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ItemListActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_items_list);
+    }
+}
