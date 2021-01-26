7b53b4fd45e04e45bb3b336d0aa5e6784ac1f675
БОРЯ ПОГРЫЗОВ <khodch3nk0@gmail.com>
2021-01-26 17:49:13 +0100

    move class and fix error
    Signed-off-by: БОРЯ ПОГРЫЗОВ <khodch3nk0@gmail.com>

--- app/src/main/java/com/example/moneytracker/ItemListActivity.java
+++ app/src/main/java/com/example/moneytracker/ItemListActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -24,30 +24,16 @@ public class ItemListActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_items_list);
-        createData();
+
+        mAdapter = new ItemsAdapter();
+
         mRecycleView = findViewById(R.id.list);
         mRecycleView.setLayoutManager(new LinearLayoutManager(this));
-        mAdapter = new ItemsAdapter(this);
+
         mRecycleView.setAdapter(mAdapter);
 
     }
 
-    private void createData() {
-        mData.add(new Record("Молоко", 35));
-        mData.add(new Record("Жизнь", 1));
-        mData.add(new Record("Курсы", 50));
-        mData.add(new Record("Хлеб", 12));
-        mData.add(new Record("Тот самый ужин,который я уплатил за всех, потому что платил картой", 600000));
-        mData.add(new Record("", 0));
-        mData.add(new Record("Тот самый ужин", 0));
-        mData.add(new Record("ракета Falcon Heavy", 1));
-        mData.add(new Record("Лего Тысячилетний сокол", 10000000));
-        mData.add(new Record("Монитор", 109));
-        mData.add(new Record("Макбук", 100));
-        mData.add(new Record("Шкаф", 100));
-        mData.add(new Record("Шоколадка", 100));
-
-    }
 
 
 }
--- app/src/main/java/com/example/moneytracker/ItemsAdapter.java
+++ app/src/main/java/com/example/moneytracker/ItemsAdapter.java
@@ -6,21 +6,25 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 
-
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ItemViewHolder> {
 
-    private List<Record> data = Collections.emptyList();
+    private List<Record> data = new ArrayList<>();
+
+    public ItemsAdapter(){
+        createData();
+    }
 
     @Override
     public ItemsAdapter.ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-       View view = LayoutInflater.from(parent.getContext())
-               .inflate(R.layout.item_record, parent, false));
-       return new ItemViewHolder(view);
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_record, parent, false);
+        return new ItemViewHolder(view);
     }
 
     @Override
@@ -35,12 +39,28 @@ class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ItemViewHolder> {
     }
 
 
+    private void createData() {
+        data.add(new Record("Молоко", 35));
+        data.add(new Record("Жизнь", 1));
+        data.add(new Record("Курсы", 50));
+        data.add(new Record("Хлеб", 12));
+        data.add(new Record("Тот самый ужин,который я уплатил за всех, потому что платил картой", 600000));
+        data.add(new Record("", 0));
+        data.add(new Record("Тот самый ужин", 0));
+        data.add(new Record("ракета Falcon Heavy", 1));
+        data.add(new Record("Лего Тысячилетний сокол", 10000000));
+        data.add(new Record("Монитор", 109));
+        data.add(new Record("Макбук", 100));
+        data.add(new Record("Шкаф", 100));
+        data.add(new Record("Шоколадка", 100));
 
-    static class  ItemViewHolder extends RecyclerView.ViewHolder {
+    }
+
+    static class ItemViewHolder extends RecyclerView.ViewHolder {
         private final TextView title;
         private final TextView price;
 
-        public ItemViewHolder( View itemView) {
+        public ItemViewHolder(View itemView) {
             super(itemView);
             title = itemView.findViewById(R.id.title);
             price = itemView.findViewById(R.id.price);
@@ -51,9 +71,6 @@ class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ItemViewHolder> {
             price.setText(String.valueOf(record.getPrice()));
         }
     }
-
-
-
 
 
 }
