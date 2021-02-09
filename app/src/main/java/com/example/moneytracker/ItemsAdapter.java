8b4463fc8c9973e86c30e5af7019a2bdbf65a66d
БОРЯ ПОГРЫЗОВ <khodch3nk0@gmail.com>
2021-02-09 17:42:18 +0100

    delete items interface delete dialog
    Signed-off-by: БОРЯ ПОГРЫЗОВ <khodch3nk0@gmail.com>

--- app/src/main/java/com/example/moneytracker/ItemsAdapter.java
+++ app/src/main/java/com/example/moneytracker/ItemsAdapter.java
@@ -1,5 +1,6 @@
 package com.example.moneytracker;
 
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,32 +16,74 @@ import java.util.List;
 class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ItemViewHolder> {
 
     private List<Item> data = new ArrayList<>();
+    private ItemsAdapterListener listener = null;
 
-    public void setData(List<Item>data){
-        this.data= data;
+    public void setListener(ItemsAdapterListener listener) {
+        this.listener = listener;
+    }
+
+    public void setData(List<Item> data) {
+        this.data = data;
         notifyDataSetChanged();
     }
 
-    public void addItem(Item item){
-        data.add(0,item);
+    public void addItem(Item item) {
+        data.add(0, item);
         notifyItemInserted(0);
     }
 
     @Override
-    public ItemsAdapter.ItemViewHolder  onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ItemsAdapter.ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View view = inflater.inflate(R.layout.item, parent, false);
         return new ItemViewHolder(view);
     }
+
     @Override
     public void onBindViewHolder(ItemsAdapter.ItemViewHolder holder, int position) {
         Item record = data.get(position);
-        holder.applyData(record);
+        holder.bind(record, position, listener, selections.get(position, false));
     }
+
     @Override
     public int getItemCount() {
         return data.size();
     }
+
+
+    private SparseBooleanArray selections = new SparseBooleanArray(); //хранит соответствие позиций (интов и буленов)
+
+    public void toggleSelection(int position) {
+        if (selections.get(position, false)) {
+            selections.delete(position);
+        } else {
+            selections.put(position, true);
+        }
+        notifyItemChanged(position);  //перересовывает список
+    }
+
+    void clearSelection() {    //убирает выделые элементы списка
+        selections.clear();
+        notifyDataSetChanged();  //перересовывает
+    }
+
+    int getSelectedItemCount() {    //считает количество выделенных
+        return selections.size();
+    }
+
+    List<Integer> getSelectedItems() {    //собрать массив выделенных позиций
+        List<Integer> items = new ArrayList<>(selections.size());
+        for (int i = 0; i < selections.size(); i++) {
+                items.add(selections.keyAt(i));
+        }
+        return items;
+    }
+    Item remove(int pos){  //удаление элементов списка
+        final Item item = data.remove(pos);
+        notifyItemRemoved(pos);
+        return item;
+    }
+
 
     static class ItemViewHolder extends RecyclerView.ViewHolder {
         private final TextView title;
@@ -52,9 +95,31 @@ class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ItemViewHolder> {
             price = itemView.findViewById(R.id.price);
         }
 
-        public void applyData(Item item) {
+        public void bind(final Item item, final int position, final ItemsAdapterListener listener, boolean selected) {
             title.setText(item.name);
             price.setText(item.price);
+
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        listener.onItemClick(item, position);
+                    }
+                }
+            });
+
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    if (listener != null) {
+                        listener.onItemLongClick(item, position);
+                    }
+                    return true;
+                }
+            });
+
+            itemView.setActivated(selected);
         }
     }
 }
--- app/src/main/java/com/example/moneytracker/ItemsFragment.java
+++ app/src/main/java/com/example/moneytracker/ItemsFragment.java
@@ -1,21 +1,28 @@
 package com.example.moneytracker;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.ActionMode;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.List;
 
 import retrofit2.Call;
@@ -51,6 +58,7 @@ public class ItemsFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         adapter = new ItemsAdapter();
+        adapter.setListener(new AdapterListener());
 
         Bundle bundle = getArguments();
         type = bundle.getString(TYPE_KEY, Item.TYPE_EXPENSES);
@@ -77,7 +85,6 @@ public class ItemsFragment extends Fragment {
         recycler.setAdapter(adapter);
 
 
-
         refresh = view.findViewById(R.id.refresh);
         refresh.setColorSchemeColors(Color.DKGRAY);
         refresh.setOnRefreshListener(this::loadItems);
@@ -89,13 +96,13 @@ public class ItemsFragment extends Fragment {
 
         call.enqueue(new Callback<List<Item>>() {
             @Override
-            public void onResponse(@NonNull Call<List<Item>> call,@NonNull Response<List<Item>> response) {
+            public void onResponse(@NonNull Call<List<Item>> call, @NonNull Response<List<Item>> response) {
                 adapter.setData(response.body());
                 refresh.setRefreshing(false);
             }
 
             @Override
-            public void onFailure(@NonNull Call<List<Item>> call,@NonNull Throwable t) {
+            public void onFailure(@NonNull Call<List<Item>> call, @NonNull Throwable t) {
                 refresh.setRefreshing(false);
             }
         });
@@ -105,10 +112,89 @@ public class ItemsFragment extends Fragment {
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if (requestCode == ADD_ITEM_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
             Item item = data.getParcelableExtra("item");
-            if (item.type.equals(type)){
+            if (item.type.equals(type)) {
                 adapter.addItem(item);
             }
         }
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    //                          ACTION MODE CODE
+
+    private ActionMode actionMode = null;
+
+    private void removeSelectedItems() {   //метод после нажатия кнопки
+        for (int i = adapter.getSelectedItems().size() - 1; i >= 0; i--) {
+            adapter.remove(adapter.getSelectedItems().get(i));
+        }
+        actionMode.finish();
+    }
+
+    private class AdapterListener implements ItemsAdapterListener {
+
+
+        private ActionMode.Callback actionModeCallback = new ActionMode.Callback() {
+            //хз зачем это из AdapterListener.OnItemClick
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                MenuInflater inflater = new MenuInflater(getContext());
+                inflater.inflate(R.menu.items_menu, menu);
+                return true;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.remove:
+                        showDialog();
+                        break;
+                }
+                return false;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+                adapter.clearSelection();
+                actionMode = null;
+            }
+        };
+
+        @Override
+        //меняем тулбар меин активити при нажатии на экшен мод для удаления фрагмента
+        public void onItemClick(Item item, int position) {
+            //проверка на выделения
+            if (isInActionMode()) {
+                toggleSelection(position);
+            }
+        }
+
+        @Override
+        public void onItemLongClick(Item item, int position) {
+            if (isInActionMode()) {
+                return;
+            }
+            actionMode = ((AppCompatActivity) getActivity()).startSupportActionMode(actionModeCallback);
+            toggleSelection(position);
+        }
+
+        //метод чтобы не нажимать повторно
+        private boolean isInActionMode() {
+            return actionMode != null;
+        }
+
+        private void toggleSelection(int position) {
+            adapter.toggleSelection(position);
+        }
+
+    }
+
+    private void showDialog() {
+        DeleteItemsDialog dialog = new DeleteItemsDialog();
+        dialog.show(getFragmentManager(),"ConfirmationDialog");
     }
 }
