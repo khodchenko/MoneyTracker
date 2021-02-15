1a80019bc3fef5c615d8812e897ff5fad79b3749
Bogdan Khodchenko <khoch3nk0@gmail.com>
2021-02-16 00:17:32 +0100

    balance screen xml in work
    Signed-off-by: Bogdan Khodchenko <khoch3nk0@gmail.com>

--- app/src/main/java/com/example/moneytracker/BalanceFragment.java
+++ app/src/main/java/com/example/moneytracker/BalanceFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,11 +12,27 @@ import androidx.fragment.app.Fragment;
 
 public class BalanceFragment extends Fragment {
 
+    private TextView total;
+    private TextView expense;
+    private TextView income;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.frragment_balance, container, false);
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        total= view.findViewById(R.id.tv_balance_total);
+        expense= view.findViewById(R.id.tv_balance_expense);
+        income= view.findViewById(R.id.tv_balance_income);
+
+    }
+
+    private void updateData(){
+
     }
 }
 
